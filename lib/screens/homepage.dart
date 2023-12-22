99f19dea7a152bf7bfba0d85db250ce1cbe74b15
mikeyolang <brunomike965@gmail.com>
2023-12-22 20:28:20 +0300

    Verification Email

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:registrastionapp/firebase_options.dart';
+import 'package:registrastionapp/views/verifyemail.dart';
+
+class MyHomepage extends StatelessWidget {
+  const MyHomepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Homepage"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user!.emailVerified) {
+                print("Verified User");
+              } else {
+                print("You need to verify");
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const VerifyEmailView(),
+                  ),
+                );
+              }
+              return const Text("Done");
+            default:
+              return const Text("Loading ...");
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/views/verifyemail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
