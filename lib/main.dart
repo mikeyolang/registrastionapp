6c7c166d8f2defd58cdbcf66384bb55a10bb5f71
mikeyolang <brunomike965@gmail.com>
2024-01-06 21:07:06 +0300

    Modified the Logout button

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ import 'firebase_options.dart';
 
 // import 'package:registrastionapp/views/registerview.dart';
 
-void main() async{
-   WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -28,11 +28,38 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const RegisterView(),
+      home: const LoginView(),
       routes: {
         "/login": (context) => const LoginView(),
         "/register": (context) => const RegisterView(),
       },
     );
   }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Sign Out"),
+          content: const Text("Are you sure you wanna Sign Out?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text("Cancel"),
+            ),
+          ],
+        );
+      }).then(
+    (value) => value ?? false,
+  );
 }
--- lib/views/notesview.dart
+++ lib/views/notesview.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
+
+import 'package:registrastionapp/main.dart';
 
 enum MenuAction { logout }
 
@@ -17,8 +21,18 @@ class _NotesViewState extends State<NotesView> {
       appBar: AppBar(
         title: const Text("Main UI"),
         actions: [
-          PopupMenuButton(onSelected: (value) {
-            devtools.log("Menu Action Value");
+          PopupMenuButton(onSelected: (value) async {
+            switch (value) {
+              case MenuAction.logout:
+                final shouldLogout = await showLogOutDialog(context);
+                if (shouldLogout) {
+                  await FirebaseAuth.instance.signOut();
+                  // ignore: use_build_context_synchronously
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil("/logout/", (_) => false);
+                }
+                break;
+            }
           }, itemBuilder: (context) {
             return [
               const PopupMenuItem<MenuAction>(
@@ -33,6 +47,3 @@ class _NotesViewState extends State<NotesView> {
     );
   }
 }
-
-
-      
