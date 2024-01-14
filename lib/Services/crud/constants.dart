5a04d5f83a81736c3a6f730f477b27231aadeed1
mikeyolang <brunomike965@gmail.com>
2024-01-14 21:41:34 +0300

    Started creting the DB

---
+++ lib/Services/crud/constants.dart
@@ -0,0 +1,9 @@
+const dbName = "notes.db";
+const noteTable = "notes";
+const userTable = "user";
+const idColumn = "id";
+const emailColumn = "email";
+
+const userIdColumn = "user_id";
+const textColumn = "text";
+const isSyncedWithCloudColumn = "is_synced_with_cloud";
