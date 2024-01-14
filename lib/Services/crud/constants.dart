61a6630d2d4e1785412f647a1105a9f9f7c59b79
mikeyolang <brunomike965@gmail.com>
2024-01-14 22:27:02 +0300

    Created the Database creation code

--- lib/Services/crud/constants.dart
+++ lib/Services/crud/constants.dart
@@ -7,3 +7,21 @@ const emailColumn = "email";
 const userIdColumn = "user_id";
 const textColumn = "text";
 const isSyncedWithCloudColumn = "is_synced_with_cloud";
+
+// creating the User Table
+const createUserTable = '''CREATE TABLE IF NOT EXISTS "user"(
+            "id"  INTEGER NOT NULL,
+            "email"  TEXT NOT NULL UNIQUE,
+            PRIMARY KEY("id" AUTOINCREMENT)
+      ); ''';
+
+// The notes Table
+const createNotesTable = ''' CREATE TABLE IF NOT EXISTS "notes"(
+          "id" INTEGER NOT NULL,
+          "user_id" INTEGER NOT NULL,
+          "text" TEXT,
+          "is_synced_with_cloud" INTEGER NOT NULL DEFAULT 0,
+          FOREIGN KEY ("user_id") REFERENCES "user"("id"),
+          PRIMARY KEY ("id" AUTOINCREMENT)
+      );
+      ''';
--- lib/Services/crud/notes_service.dart
+++ lib/Services/crud/notes_service.dart
@@ -1,13 +1,49 @@
+// ignore_for_file: unnecessary_null_comparison
+
 import 'package:flutter/foundation.dart';
 import 'package:registrastionapp/Services/crud/constants.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
 
+class DatabaseAlreadyOpenedException implements Exception {}
+
+class UnableToGetDocumentDirectory implements Exception {}
+
+class DatabaseIsNotOpenException implements Exception {}
+
 // Openning the DataBase
-class NoteService{
-  Future<void> openDb() async{
-    
+class NoteService {
+  Database? db0;
+
+  Future<void> closeDb() async {
+    ValueGetter db = db0;
+    if (db == null) {
+      throw DatabaseIsNotOpenException();
+    } else {
+      await db.close();
+      db = null;
+    }
+  }
+
+  Future<void> openDb() async {
+    if (db0 != null) {
+      throw DatabaseAlreadyOpenedException();
+    }
+    try {
+      final docsPath = await getApplicationCacheDirectory();
+      final dbPath = join(docsPath.path, dbName);
+      final db = await openDatabase(dbPath);
+      db0 = db;
+      // Create User Table
+
+      await db.execute(createUserTable);
+
+      //  Creating the Notes Table
+      await db.execute(createNotesTable);
+    } on MissingPlatformDirectoryException {
+      throw UnableToGetDocumentDirectory();
+    }
   }
 }
 
